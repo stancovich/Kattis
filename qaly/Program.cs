226329f4747dd77546deb16e28f26c076061de7b
max <hello@max.com>
2021-11-06 17:25:18 +0100

    solved qaly

---
+++ qaly/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace qaly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            decimal sumQaly = 0;
+            string[] input = new string[N];
+            for (int i = 0; i < N; i++)
+            {
+                input[i] = Console.ReadLine();
+            }
+            int[] numbers = new int[N];
+            for (int i = 0; i < N; i++)
+            {
+                string[]temp = input[i].Split(' ');
+                decimal result = decimal.Parse(temp[0]) * decimal.Parse(temp[1]);
+                sumQaly += result;
+            }
+            Console.WriteLine(sumQaly);
+        }
+    }
+}
