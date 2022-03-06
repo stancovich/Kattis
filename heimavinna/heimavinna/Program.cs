ac5229c7582640312d143cc3d0181a29378b2229
Simon <Simon.max.stancovich@gmail.com>
2022-03-06 15:02:59 +0100

    solved heimavinna

---
+++ heimavinna/heimavinna/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace heimavinna
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] splitInput = SplitInput(input);
+            int problems = CountSpots(splitInput);
+            Console.WriteLine(problems);
+        }
+        static string[] SplitInput(string input)
+        {
+            return input.Split(';');
+        }
+        static int CountSpots(string[] input)
+        {
+            int count = 0;
+            foreach(string s in input)
+            {
+                if (s.Contains("-"))
+                {
+                    string[] holder = s.Split('-');
+                    int a = int.Parse(holder[0]);
+                    int b = int.Parse(holder[1]);
+                    for(int i = a; i <= b; i++)
+                    {
+                        count++;
+                    }
+                }
+                else
+                {
+                    count++;
+                }
+                
+            }
+            return count;
+        }
+    }
+}
