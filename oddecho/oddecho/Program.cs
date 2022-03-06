28093add1cc8cbcdba4c222d8677ef36bb3f69bd
Simon <Simon.max.stancovich@gmail.com>
2022-03-06 16:30:46 +0100

    solved oddecho/

---
+++ oddecho/oddecho/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace oddecho
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lines = int.Parse(Console.ReadLine());
+            for(int i = 0; i < lines; i++)
+            {
+                string input = Console.ReadLine();
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(input);
+                }
+            }
+        }
+    }
+}
