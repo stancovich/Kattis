cdaa14ab3cd0df20a38d000c1930d2008b36d7ec
Simon <Simon.max.stancovich@gmail.com>
2022-03-06 18:34:29 +0100

    solved triarea

---
+++ twostones/twostones/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace twostones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int.TryParse(Console.ReadLine(), out int input);
+            Console.WriteLine(input % 2 != 0 ? "Alice" : "Bob");
+        }
+    }
+}
