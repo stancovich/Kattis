770076d2085bdd980dded9e7bacb63e4896356c0
Simon <Simon.max.stancovich@gmail.com>
2022-03-06 18:21:58 +0100

    solved triarea

---
+++ triarea/triarea/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace triarea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            decimal triangleArea = CalculateTriangleArea(input);
+            Console.WriteLine(triangleArea);
+
+        }
+        static decimal CalculateTriangleArea(string input)
+        {
+            string[] inputArray = input.Split(" ");
+            decimal.TryParse(inputArray[0], out decimal h);
+            decimal.TryParse(inputArray[1], out decimal b);
+            decimal triangleArea = (h * b) / 2;
+            return triangleArea;
+        }
+    }
+}
