54068030fac8581cc707a5d704fdfc1e49bff179
Simon <Simon.max.stancovich@gmail.com>
2022-03-06 14:25:27 +0100

    solved autori

---
+++ autori/autori/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace autori
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string[] slicedInput = Slice(input);
+            string FirstLetterOfAllNames = AddFirstLetterOfName(slicedInput);
+
+            Console.WriteLine(FirstLetterOfAllNames);
+
+        }
+        static string[] Slice (string input)
+        {
+            string[] stringArray = input.Split('-');
+
+            return stringArray;
+        }
+        static string AddFirstLetterOfName(string[] input)
+        {
+            string output = "";
+            foreach (string name in input)
+            {
+                output += name[0];
+            }
+            return output;
+        }
+    }
+}
