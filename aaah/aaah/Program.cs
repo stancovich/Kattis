3327dad95ef1cc915cc6b28da4f5d208ede9bb1b
Simon <Simon.max.stancovich@gmail.com>
2022-03-13 15:48:16 +0100

    Solved aaah

---
+++ aaah/aaah/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace aaah
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string doctorInput = Console.ReadLine();
+            Console.WriteLine(NoOrGo(input, doctorInput));
+        }
+        static string NoOrGo(string input, string doctorInput)
+        {
+            return input.Length >= doctorInput.Length ? "go" : "no";
+        }
+    }
+}
