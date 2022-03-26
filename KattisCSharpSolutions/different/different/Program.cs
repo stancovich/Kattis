4c80a256399c8013dbb47b9eeca4c4308f2a57a4
Simon <Simon.max.stancovich@gmail.com>
2022-03-26 09:50:07 +0100

    solved different

---
+++ KattisCSharpSolutions/different/different/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace different
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                string[] input = line.Split(' ');
+                int.TryParse(input[0], out int num1);
+                int.TryParse(input[1], out int num2);
+                int output = Math.Abs(num1 - num2);
+                Console.WriteLine(output);
+            }
+        }
+    }
+}
