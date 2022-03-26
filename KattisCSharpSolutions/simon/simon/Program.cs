62f2f551f0df8ead8ddc2d06889c04fbc30fd2a5
Simon <Simon.max.stancovich@gmail.com>
2022-03-26 11:08:00 +0100

    solved simonsays

---
+++ KattisCSharpSolutions/simon/simon/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace simon
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int testCases = Int32.Parse(Console.ReadLine());
+
+            var print = new List<string>();
+
+            while (testCases-- > 0)
+            {
+                var line = Console.ReadLine();
+                print.Add(line.StartsWith("simon says ") ? line.Remove(0, 11) : "");
+            }
+            foreach (var simonSaid in print)
+                Console.WriteLine(simonSaid);
+        }
+    }
+}
